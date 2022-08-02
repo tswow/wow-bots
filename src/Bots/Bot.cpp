19ad65c0a65e9dc13d405e07aada0a7d66d9dd1a
ihm-tswow <ihm-ts@protonmail.com>
2022-08-02 12:39:17 +0200

    fire OnCreate event

--- src/Bots/Bot.cpp
+++ src/Bots/Bot.cpp
@@ -36,6 +36,7 @@ Bot::Bot(BotThread* thread, std::string const& username, std::string const& pass
 {
     std::transform(m_username.begin(), m_username.end(), m_username.begin(), [](uint8_t c) { return std::toupper(c); });
     std::transform(m_password.begin(), m_password.end(), m_password.begin(), [](uint8_t c) { return std::toupper(c); });
+    FIRE(OnCreate, this->GetEvents(), {}, *this);
 }
 
 std::string const& Bot::GetUsername() const
